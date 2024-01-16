05c3574ca0423eb6235c83ba63b5c9dd49e40e0c
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2024-01-16 13:03:52 +0530

    update api key

--- recipe-book/script.js
+++ recipe-book/script.js
@@ -1,4 +1,4 @@
-const API_KEY = "275d58779ccf4e22af03e792e8819fff";
+const API_KEY = "9dcc4d02c252f5ffbbef54a2d7baf886ce09d36d";
 const recipeListEl = document.getElementById("recipe-list");
 
 function displayRecipes(recipes) {
